2df890cd23f873f17e846bca6527b7aef40adc5b
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-06-27 03:16:47 -0300

    changed getTimeline route request

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.twitter.com/1.1/statuses/',
+  baseURL: 'http://localhost:3333/',
   timeout: 10000
 })
 
 export function getTimeline() {
   return api
-    .get('home_timeline.json', {
-      headers: {
-        'consumer-key': process.env.CONSUMER_KEY,
-        'consumer-secret': process.env.CONSUMER_SECRET,
-        'token': process.env.TOKEN,
-        'token-secret': process.env.TOKEN_SECRET
-      }
-    })
-    .then(result => result.data)
-}
-
-export function getGithub() {
-  return api
-    .get('https://api.github.com/users/devmozao')
+    .get('getTimeline')
     .then(result => result.data)
 }
