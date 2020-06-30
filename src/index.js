b1aed9f38ea4fa6853b7fef9da5a1957c041ec00
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-06-29 22:44:59 -0300

    added Container styled component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './index.css'
+import styled from 'styled-components'
 
 import Routes from './routes'
 
+const Container = styled.div`
+  /* margin: 20px 20px 20px 20x !important; */
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-left: 10%;
+  margin-right: 10%;
+  background-color: #1D2227;
+`
+
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Container id='teste'>
+      <Routes />
+    </Container>
   </React.StrictMode>,
   document.getElementById('root')
 )
