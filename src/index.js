144d775827220f1a16841266f3e67d3ac77be3e6
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-06-30 00:11:04 -0300

    fixed colors and removed comments

--- src/index.js
+++ src/index.js
@@ -2,28 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Reset } from 'styled-reset'
+import 'victormono'
+
 import styled from 'styled-components'
+
+import colors from './services/colors'
 
 import Routes from './routes'
 
+const IndexWrapper = styled.div`
+  background-color: ${colors.gray};
+`
+
 const Container = styled.div`
-  /* margin: 20px 20px 20px 20x !important; */
   max-width: 1280px;
   height: 100vh;
   margin-left: auto;
   margin-right: auto;
   margin-left: 10%;
   margin-right: 10%;
-  background-color: #1D2227;
+  background-color: ${colors.darkGray};
   color: #fefefe;
+  font-family: 'JetBrains Mono';
+  font-size: 16px;
 `
 
 ReactDOM.render(
   <React.StrictMode>
-    <Reset />
-    <Container id='teste'>
-      <Routes />
-    </Container>
+    <IndexWrapper>
+      <Reset />
+      <Container>
+        <Routes />
+      </Container>
+    </IndexWrapper>
   </React.StrictMode>,
   document.getElementById('root')
 )
