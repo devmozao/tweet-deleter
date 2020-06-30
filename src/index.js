e895ea7f069a69ea66d04c1e18920527ae0c0c83
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-06-29 22:55:41 -0300

    added text color and default height container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Reset } from 'styled-reset'
 import styled from 'styled-components'
 
 import Routes from './routes'
@@ -8,15 +9,18 @@ import Routes from './routes'
 const Container = styled.div`
   /* margin: 20px 20px 20px 20x !important; */
   max-width: 1280px;
+  height: 100vh;
   margin-left: auto;
   margin-right: auto;
   margin-left: 10%;
   margin-right: 10%;
   background-color: #1D2227;
+  color: #fefefe;
 `
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
     <Container id='teste'>
       <Routes />
     </Container>
