57ac7d0f11d4c15c1a1cfa9c63ade16cae6ee12c
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-07-20 21:16:13 -0300

    wip in tests

---
+++ src/__tests__/tweets.test.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+import { render, fireEvent, waitFor, waitForElement, screen } from '@testing-library/react'
+
+import Tweets from '../containers/tweets'
+
+const payload = {
+  data: [{
+    created_at: 'Thu Jul 09 00:07:09 +0000 2020',
+    id: 1281017008608415700,
+    id_str: '1281017008608415746',
+    text: '@tchublim to te aguardando! chega aí!',
+    truncated: false,
+    entities: { hashtags: [], symbols: [], user_mentions: [{ screen_name: 'tchublim', name: 'يوشا', id: 3896095762, id_str: '3896095762', indices: [0, 9] }], urls: [] },
+    source: '<a href="https://about.twitter.com/products/tweetdeck" rel="nofollow">TweetDeck</a>',
+    in_reply_to_status_id: 1281014365169299500,
+    in_reply_to_status_id_str: '1281014365169299458',
+    in_reply_to_user_id: 3896095762,
+    in_reply_to_user_id_str: '3896095762',
+    in_reply_to_screen_name: 'tchublim',
+    user: { id: 1194770843445989400, id_str: '1194770843445989376', name: 'mozaoJSX', screen_name: 'devMozao', location: 'Osasco/SP', description: '💜 Dev FrontEnd React, JS lover 💚\n🚀 Webmaster no @webmasters_devs ✨\n🛰️ dev streamer no https://t.co/NBjtbpZS74 🌎', url: 'https://t.co/dbzocRaAiO', entities: { url: { urls: [{ url: 'https://t.co/dbzocRaAiO', expanded_url: 'https://github.com/devMozao', display_url: 'github.com/devMozao', indices: [0, 23] }] }, description: { urls: [{ url: 'https://t.co/NBjtbpZS74', expanded_url: 'http://twitch.tv/devmozao', display_url: 'twitch.tv/devmozao', indices: [86, 109] }] } }, protected: false, followers_count: 1671, friends_count: 1138, listed_count: 9, created_at: 'Thu Nov 14 00:15:55 +0000 2019', favourites_count: 12860, utc_offset: null, time_zone: null, geo_enabled: false, verified: false, statuses_count: 6497, lang: null, contributors_enabled: false, is_translator: false, is_translation_enabled: false, profile_background_color: 'F5F8FA', profile_background_image_url: null, profile_background_image_url_https: null, profile_background_tile: false, profile_image_url: 'http://pbs.twimg.com/profile_images/1236773797824548866/DtlQynBU_normal.jpg', profile_image_url_https: 'https://pbs.twimg.com/profile_images/1236773797824548866/DtlQynBU_normal.jpg', profile_banner_url: 'https://pbs.twimg.com/profile_banners/1194770843445989376/1584399228', profile_link_color: '1DA1F2', profile_sidebar_border_color: 'C0DEED', profile_sidebar_fill_color: 'DDEEF6', profile_text_color: '333333', profile_use_background_image: true, has_extended_profile: true, default_profile: true, default_profile_image: false, following: false, follow_request_sent: false, notifications: false, translator_type: 'none' },
+    geo: null,
+    coordinates: null,
+    place: null,
+    contributors: null,
+    is_quote_status: false,
+    retweet_count: 0,
+    favorite_count: 0,
+    favorited: false,
+    retweeted: false,
+    lang: 'pt'
+  }],
+  remaining: '899'
+}
+
+it('Should render the tweet', () => {
+  const component = render(<Tweets tweetdata={payload} />)
+  expect(component.getByText('@tchublim to te aguardando! chega aí!'))
+})
+
+it('Should select the tweet and change the dom with the selected effect', () => {
+  const component = render(<Tweets tweetdata={payload} />)
+
+  fireEvent.click(component.getByText('@tchublim to te aguardando! chega aí!'))
+
+  expect(component.getByTestId(payload.data[0].id_str.toString()))
+})
