6d49b43f9c215d96918f1bbee29558d5c997b129
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-08-04 00:08:08 -0300

    updated label value

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <p>Home</p>
       <a href='/login'>
-        Twitter
-        </a>
+        Login with Twitter
+      </a>
     </>
   )
 }
